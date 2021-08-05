e75dc685ddd597038fb7b47b4b10ab0da0cc6236
Bakhodir Radjabov <bakhodir.radjabov@gmail.com>
2021-08-05 17:13:52 +0500

    Add project files.

---
+++ Adapter.Task1/ContainerFromElements.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter.Task1
+{
+    public class ContainerFromElements<T> : IContainer<T>
+    {
+
+        private readonly IElements<T> elements;
+
+        public ContainerFromElements(IElements<T> elements)
+        {
+            this.elements = elements;
+        }
+
+        public IEnumerable<T> Items => elements.GetElements();
+
+        public int Count => GetElementsCount();
+
+        public int GetElementsCount()
+        {
+            int result = 0;
+            using (IEnumerator<T> enumerator = elements.GetElements().GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                    result++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Adapter.Task1/MyPrinterAdapter.cs
@@ -0,0 +1,19 @@
+﻿namespace Adapter.Task1
+{
+    public class MyPrinterAdapter : IMyPrinter
+    {
+        private readonly Printer _printer;
+
+        public MyPrinterAdapter(Printer printer)
+        {
+            _printer = printer;
+        }
+
+        public void Print<T>(IElements<T> elements)
+        {
+            var containerFromElements = new ContainerFromElements<T>(elements);
+
+            _printer.Print(containerFromElements);
+        }
+    }
+}
